fb74aa95951b8634c7dedb2dac62d295d9bca7ec
nmuenche <nick.muenchen@capgemini.com>
2020-11-19 09:55:08 -0500

    Fixed problems and added usage guide

--- obj/Debug/net48/App.g.i.cs
+++ obj/Debug/net48/App.g.i.cs
@@ -45,7 +45,7 @@ namespace WPFLateTimesheetsApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -66,7 +66,7 @@ namespace WPFLateTimesheetsApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public static void Main() {
             WPFLateTimesheetsApp.App app = new WPFLateTimesheetsApp.App();
             app.InitializeComponent();
